fd9d0676e1fcb0424300d727ad843c9ca8dfb14d
Iryna Stepanets <irusya.stepanets@gmail.com>
2025-02-13 17:05:13 +0100

    lesson-6_irynas

---
+++ lesson-6/task1.js
@@ -0,0 +1,26 @@
+// Створіть функцію яка приймає два параметри: width і height.
+// Усередині функції обчисліть площу прямокутника, перемноживши width на height та поверніть результат з функції.
+// Викличте вашу функцію з аргументами (наприклад 5 і 10 і виведіть результат (площу прямокутника) в консоль.
+// Реалізуйте функцію трьома способами (function declaration, function expression, arrow function)
+
+const square = (width, height) => {
+    return width * height;
+};
+
+console.log("Площа прямокутника -", square(5, 10));
+
+
+
+function square1 (width, height){
+    return width * height;
+
+}
+console.log("Площа прямокутника -", square1(6, 18));
+
+
+
+const square2 = function (width, height){
+        return width * height;
+
+}
+console.log("Площа прямокутника -", square2(3, 4));
