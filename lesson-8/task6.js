68c096705281bfaf581023bae8dd1daa6746a38f
Iryna Stepanets <irusya.stepanets@gmail.com>
2025-02-20 17:45:16 +0100

    bug fixes

--- lesson-8/task6.js
+++ lesson-8/task6.js
@@ -9,8 +9,12 @@
 
 const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
 
-numbersList.sort((a, b) => a - b);
+const copyNumberList = [...numbersList];
+
+copyNumberList.sort((a, b) => a - b);
 
 console.log(numbersList);
+
+console.log(copyNumberList);
 
 
